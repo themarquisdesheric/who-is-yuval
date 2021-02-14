31908a28a3109ffd9eeda2fb9a208d2bc6d9a750
Yuval Allweil <themarquisdesheric@gmail.com>
2021-02-13 19:18:54 -0800

    add language percentages to chart labels, destroy on unmount

--- src/components/GithubDashboard.svelte
+++ src/components/GithubDashboard.svelte
@@ -5,28 +5,34 @@
   const langPercentages = {
     "JavaScript": 80,
     "Shell": 9,
+    "TypeScript": 9,
     "Python": 1,
-    "TypeScript": 9
-  }  
+  }
+  const labels = Object.entries(langPercentages).map(([key, value]) =>
+    `${key} (${value}%)`
+  )
+
   let ctx;
 
   onMount(() => {
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: Object.keys(langPercentages),
-            datasets: [{
-              data: Object.values(langPercentages),
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-              ],
-              borderWidth: 1
-            }]
-        },
-      })
+    const chart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels,
+          datasets: [{
+            data: Object.values(langPercentages),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+            ],
+            borderWidth: 1
+          }]
+      },
+    })
+    
+    return () => chart.destroy()
   })
 </script>
 
@@ -39,6 +45,7 @@
 <style>
   section {
     height: 100vh;
+    background-color: rgba(0, 0, 255, 0.2);
   }
   
   @media (min-width: 1300px) {
