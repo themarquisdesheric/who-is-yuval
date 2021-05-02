61eba1ca54b47ce56d93b423d82dcf32366f3edc
Yuval Allweil <themarquisdesheric@gmail.com>
2021-05-01 23:14:43 -0700

    work logos looking good

--- src/routes/work/index.svelte
+++ src/routes/work/index.svelte
@@ -23,8 +23,15 @@
     <h1 class="text-4xl font-extrabold">Work</h1>
     <p>I work as a frontend developer for Kroger, where I bring to life beautiful and intuitive interfaces.</p>
     <p>In the past I've done Conversion Rate Optimization for The Good, worked in several positions at fast-paced startups, and moonlighted as a UX consultant.</p>
-    <p>Here are some of the websites I’ve worked on:</p>
-    <div><p>logos...</p></div>
+    <p class="mb-8">Here are some of the websites I’ve worked on:</p>
+    <div class="logos">
+      <img src="/work/kroger.png" />
+      <img src="/work/theo.png" />
+      <img src="/work/dotandbo.png" />
+      <img src="/work/apache.png" />
+      <img src="/work/snowpeak.png" />
+      <img src="/work/deathwish.png" />
+    </div>
   </section>
   <section>
     <h2 class="text-4xl font-extrabold">Projects</h2>
@@ -76,6 +83,36 @@
     margin-bottom: 4rem;
   }
 
+  .logos > img {
+    height: 40px;
+    justify-self: center;
+    align-self: center;
+  }
+
+  img[src="/work/kroger.png"] { grid-area: kroger; }
+  img[src="/work/theo.png"] { grid-area: theo; }
+  img[src="/work/dotandbo.png"] {
+    grid-area: dotandbo;
+    height: 20px;
+  }
+  img[src="/work/apache.png"] { grid-area: apache; }
+  img[src="/work/snowpeak.png"] { grid-area: snowpeak; }
+  img[src="/work/deathwish.png"] { grid-area: deathwish; }
+
+  .logos {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    /* if grid available */
+    display: grid;
+    row-gap: 1rem;
+    column-gap: 1rem;
+    grid-template-areas: 
+      "kroger theo"
+      "dotandbo apache"
+      "snowpeak deathwish";
+  }
+
   .most-popular-repo a > p {
     margin-bottom: 0;
   }
@@ -88,7 +125,22 @@
     bottom: 1px;
   }
 
-  p {
+  p:not(.mb-8) {
     margin: 1rem 0;
+  }
+
+  @media (min-width: 400px) {
+    .logos {
+      grid-template-areas: 
+        "kroger theo dotandbo"
+        "apache snowpeak deathwish";
+    }
+  }
+
+  @media (min-width: 700px) {
+    .logos {
+      grid-template-areas: 
+        "kroger theo dotandbo apache snowpeak deathwish";
+    }
   }
 </style>
